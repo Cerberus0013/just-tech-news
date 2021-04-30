1cdf9e43a422bf3fe2bd4486d9640af30420eacb
Breedlove13 <b.breedlove@outlook.com>
2021-04-30 15:11:38 -0700

    helpers working

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -50,7 +50,78 @@ router.get("/", (req, res) => {
       // pass a single post object into the homepage templat
       const posts = dbPostData.map(post => post.get({plain:true}))
       console.log(dbPostData[0]);
-      res.render("homepage", { posts });
+      res.render("homepage", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get('/login', (rea, res) => {
+    res.render('login');
+})
+
+router.get("/", (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect("/");
+    return;
+  }
+
+  res.render("login");
+ // other logic...
+});
+
+router.get("/post/:id", (req, res) => {
+  cPost
+    .findOne({
+      where: {
+        id: req.params.id,
+      },
+      attributes: [
+        "id",
+        "post_url",
+        "title",
+        "created_at",
+        [
+          sequelize.literal(
+            "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+          ),
+          "vote_count",
+        ],
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            "id",
+            "comment_text",
+            "post_id",
+            "user_id",
+            "created_at",
+          ],
+          include: {
+            model: User,
+            attributes: ["username"],
+          },
+        },
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "No post found with this id" });
+        return;
+      }
+
+      // serialize the data
+      const post = dbPostData.get({ plain: true });
+
+      // pass data to template
+      res.render("single-post", { post, loggedIn: req.session.loggedIn });
     })
     .catch((err) => {
       console.log(err);
--- server.js
+++ server.js
@@ -5,7 +5,9 @@
    const hbs = exphbs.create({});
    const app = express();
    const PORT = process.env.PORT || 3000;
+  const helpers = require("./utils/helpers");
 
+   const hbs = exphbs.create({helpers})
 
    app.engine("handlebars", hbs.engine);
    app.set("view engine", "handlebars");
@@ -14,7 +16,24 @@
    app.use(express.urlencoded({ extended: true }));
     app.use(express.static(path.join(__dirname, "public")));
    // turn on routes
-   app.use(routes);
+   app.use(routes)
+   
+
+   const session = require("express-session");
+
+   const SequelizeStore = require("connect-session-sequelize")(session.Store);
+
+   const sess = {
+     secret: "Super secret secret",
+     cookie: {},
+     resave: false,
+     saveUninitialized: true,
+     store: new SequelizeStore({
+       db: sequelize,
+     }),
+   };
+
+   app.use(session(sess));
 
    // turn on connection to db and server
    sequelize.sync({ force: false }).then(() => {
